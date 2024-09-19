68994e6d3ab07f933c45516e5cd67527574f2223
Aarush-07 <aarushvs166@gmail.com>
2024-09-19 15:48:08 +0530

    BioData.c

---
+++ BioData.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Student Name : AARUSH VS \n");
+    printf("DOB          : 01/03/2007\n");
+    printf("Religion     : Hindu\n");
+    printf("Department   : B.E CSE\n");
+    printf("Father Name  : VINU KUMAR S\n");
+    printf("Mother Name  : SHEEJA R\n");
+    printf("State        : KERALA\n");
+    printf("District     : THIRUVANANTHAPURAM PO-695506\n");
+    printf("PH           : 8891850995\n");
+
+    return 0;
+}
